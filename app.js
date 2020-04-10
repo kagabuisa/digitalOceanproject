9aa5ef09f261c08650d8a4fd87c4bb1837d9fce8
Kagabu Isa <kagabuisa@gmail.com>
2020-04-10 14:00:10 +0300

    2nd commit

--- app.js
+++ app.js
@@ -5,34 +5,32 @@
 // let newCurDate = Math.floor(date.date2()+10);
 
 function getSnapshotList() {
+
     var myHeaders = new Headers();
     myHeaders.append("Authorization", "Bearer b55f0a04ae134281f185d86f0e2f5456baa43a6dfddb7edc3cbf30a5877c1985");
     myHeaders.append("Cookie", "__cfduid=dcea941712d2e0d1fbac975de663b92ac1586247452");
-
     var requestOptions = {
         method: 'GET',
         headers: myHeaders,
         redirect: 'follow'
     };
-
-    fetch("https://api.digitalocean.com/v2/snapshots?page=1&per_page=100&resource_type=droplet", requestOptions)
+    const httpReqlink = "https://api.digitalocean.com/v2/snapshots?page=1&per_page=100&resource_type=droplet"
+    const dataFetch = fetch(httpReqlink, requestOptions)
         .then(response => {
             return response.json()
         })
-        .then(medina => {
-            console.log(medina)
-            let xny = []
-            for (let i = 0; i < medina.snapshots.length; i++) {
+
+    dataFetch.then(data => {
+            for (let i = 0; i < data.snapshots.length; i++) {
                 //if(days>1){delete snapshot(create function to delete snapshot of matching ID)}
-                //https://www.codecademy.com/forum_questions/50c207bd55df51ff27004775 (create object from loop)
-                let x = medina.snapshots[i].created_at.slice(0, 10)
-                let y = medina.snapshots[i].id
+                let x = data.snapshots[i].created_at.slice(0, 10)
+                let y = data.snapshots[i].id
                 if (checkAge(x) > 2) {
                     console.log(`Snapshot ID ${y} created on ${x} is ${checkAge(x)} days old and should be deleted`)
                 } else {
                     console.log(`Snapshot ID ${y} created on ${x} is ${checkAge(x)} days old and should be kept`)
                 }
-                //  xny[i]={x:medina.snapshots[i].created_at.slice(0, 10),y:medina.snapshots[i].id};
+
             }
         })
         .catch(error => console.log('error', error));
